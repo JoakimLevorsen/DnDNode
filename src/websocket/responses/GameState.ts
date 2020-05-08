254ba42d62f9da4af5f80ea2db276420591fccc0
Joakim <joakim@kvalifik.dk>
2020-05-08 09:05:13 +0200

    Fix to new GameState

--- src/websocket/responses/GameState.ts
+++ src/websocket/responses/GameState.ts
@@ -1,30 +1,43 @@
-import { Campaign } from "./Campaigns";
+import { Campaign } from './Campaigns';
 
 interface GameStateCampaign extends Campaign {
-  dungeonMaster: string;
+    dungeonMaster: string;
 }
 
 export interface GameState {
-  characters: Array<{
-    owner: string;
-    campaign: number;
-    cRace: string;
-    cClass: string;
-    name: string;
-    ID: number;
-    xp: number;
-    level: number;
-    turnIndex: number;
-  }>;
-  diceRolls: {
-    [index: number]: Array<{
-      ID?: number;
-      diceType: number;
-      roll: number;
-      date: string;
+    myCharacters: Array<{
+        owner: string;
+        campaign: number;
+        cRace: string;
+        cClass: string;
+        name: string;
+        ID: number;
+        health: number;
+        xp: number;
+        level: number;
+        turnIndex: number;
     }>;
-  };
-  me: string;
-  ownedCampaigns: Array<GameStateCampaign>;
-  joinedCampaigns: Array<GameStateCampaign>;
+    encounteredCharacters: Array<{
+        owner: string;
+        campaign: number;
+        cRace: string;
+        cClass: string;
+        name: string;
+        ID: number;
+        health: number;
+        xp: number;
+        level: number;
+        turnIndex: number;
+    }>;
+    diceRolls: {
+        [index: number]: Array<{
+            ID?: number;
+            diceType: number;
+            roll: number;
+            date: string;
+        }>;
+    };
+    me: string;
+    ownedCampaigns: Array<GameStateCampaign>;
+    joinedCampaigns: Array<GameStateCampaign>;
 }
